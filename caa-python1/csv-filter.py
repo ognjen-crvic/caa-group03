a63d057223f5e94fdf2fd5b37d99787ef19784a3
Ognjen Crvic <crvicognjen@gmail.com>
2023-06-02 12:30:13 +0200

    Added filter to remove double entries

---
+++ caa-python1/csv-filter.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+print("test")
+
+df = pd.read_csv("../datasource/etherscan-best-pub-trans-all.csv")
+
+print(df.duplicated())
+df = df.drop_duplicates(subset=["Txhash"])
+
+print(df.duplicated())
+
+df.to_csv("../datasource/etherscan-best-pub-trans.csv", index=False)
