3dc444b1c105a784e764220f11277dcbb60812f6
Rene Steinmaßl <e1106495@student.tuwien.ac.at>
2023-06-06 00:54:58 +0200

    implement basic examples in flask using neo4j and py2neo

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ docker pull neo4j:latest
 # start the container
 docker run --name testneo4j -p7474:7474 -p7687:7687 -d \
             -v $HOME/neo4j/data:/data -v $HOME/neo4j/logs:/logs -v $HOME/neo4j/import:/var/lib/neo4j/import -v $HOME/neo4j/plugins:/plugins \ 
-            --env NEO4J_AUTH=neo4j/password  neo4j:latest
+            --env NEO4J_AUTH=neo4j/password --env NEO4JLABS_PLUGINS='["graph-data-science"]' neo4j:latest
 ```
 _Works for WSL or Bash terminal, for CMD/Powershell might need to replace $HOME variable._
 
@@ -32,7 +32,7 @@ docker cp datasource/etherscan-best-pub-trans-all.csv <name_of_neo4j_container>:
 In the neo4j console use the following command to load the data:
 
 ```cypher
-LOAD CSV FROM "file:///etherscan-best-pub-trans.csv" as row
+LOAD CSV FROM "file:///etherscan-best-pub-trans-all.csv" as row
 MERGE (a:Address {value:row[4]})
 MERGE (b:Address {value:row[5]})
 MERGE (a)-[:Transaction {tx_hash:row[0], block_num:row[1], unix_ts: row[2], datetime:row[3], amount:row[6], method:row[7]}]->(b)
---
+++ caa-python/app.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from neo4j import GraphDatabase, Result
+from py2neo import Graph
+from py2neo.cypher import Cursor
+
+app = Flask(__name__)
+
+
+# Use neo4j (GraphDatabase)
+@app.route('/example1')
+def example_1():
+    _example = Example("bolt://localhost:7687", "neo4j", "password")
+    _example.close()
+    return _example.print_example()
+
+
+# Use py2neo
+@app.route('/example2')
+def example_2():
+    _example_graph: Graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
+    res: Cursor = _example_graph.run("MATCH (a:Address)<-[t:Transaction]-(b:Address) "
+                                     "WHERE a.value=~'0x0000.*' "
+                                     "return a,t,b")
+    return res.data()  # get JSON-serializable data for testing
+
+
+class Example:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        self.driver.verify_connectivity()
+
+    def close(self):
+        self.driver.close()
+
+    def print_example(self):
+        with self.driver.session() as session:
+            res = session.execute_read(self._read_example_query)
+        return res
+
+    @staticmethod
+    def _read_example_query(tx):
+        res: Result = tx.run("MATCH (a:Address)<-[t:Transaction]-(b:Address) "
+                             "WHERE a.value=~'0x0000.*' "
+                             "return a,t,b")
+        return res.data()  # get JSON-serializable data for testing
+
+
+if __name__ == '__main__':
+    app.run()
