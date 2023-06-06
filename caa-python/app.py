f162fe9b0da9b10383e8fbc0f75857adf935ee50
Rene Steinmaßl <e1106495@student.tuwien.ac.at>
2023-06-06 23:48:12 +0200

    add graphdatascience example

--- caa-python/app.py
+++ caa-python/app.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from neo4j import GraphDatabase, Result
+from pandas import DataFrame
 from py2neo import Graph
 from py2neo.cypher import Cursor
+from graphdatascience import GraphDataScience
 
 app = Flask(__name__)
+
+HOST = "bolt://localhost:7687"
+USER = "neo4j"
+PASSWORD = "password"
 
 
 # Use neo4j (GraphDatabase)
 @app.route('/example1')
 def example_1():
-    _example: Example = Example("bolt://localhost:7687", "neo4j", "password")
+    _example: Example = Example(HOST, USER, PASSWORD)
     _example.close()
     return _example.print_example()
 
@@ -17,11 +23,21 @@ def example_1():
 # Use py2neo
 @app.route('/example2')
 def example_2():
-    _example_graph: Graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
+    _example_graph: Graph = Graph(HOST, auth=(USER, PASSWORD))
     res: Cursor = _example_graph.run("MATCH (a:Address)<-[t:Transaction]-(b:Address) "
                                      "WHERE a.value=~'0x0000.*' "
                                      "return a,t,b")
     return res.data()  # get JSON-serializable data for testing
+
+
+# Use graphdatascience
+@app.route('/example3')
+def example_3():
+    gds: GraphDataScience = GraphDataScience(HOST, auth=(USER, PASSWORD))
+    df: DataFrame = gds.run_cypher("MATCH (a:Address)<-[t:Transaction]-(b:Address) "
+                                   "WHERE a.value=~'0x0000.*' "
+                                   "return a,t,b")
+    return df.to_json()
 
 
 class Example:
