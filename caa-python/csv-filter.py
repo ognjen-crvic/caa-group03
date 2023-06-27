25428c4fd7ab00a400fa7c1e80cc1449988f1bf5
Rene Steinmaßl <e1106495@student.tuwien.ac.at>
2023-06-27 21:19:20 +0200

    filter nodes with degree >= 2

--- caa-python/csv-filter.py
+++ caa-python/csv-filter.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from pandas import DataFrame
 
 print("test")
 
-df = pd.read_csv("../datasource/etherscan-best-pub-trans-all.csv")
+df: DataFrame = pd.read_csv("../datasource/etherscan-best-pub-trans-all.csv")
+degree_2_df: DataFrame = pd.read_csv("../datasource/sorted_nodes.csv")
 
 print(df.duplicated())
 df = df.drop_duplicates(subset=["Txhash"])
@@ -10,3 +12,12 @@ df = df.drop_duplicates(subset=["Txhash"])
 print(df.duplicated())
 
 df.to_csv("../datasource/etherscan-best-pub-trans.csv", index=False)
+
+decomposed_df: DataFrame = pd.DataFrame(columns=df.columns.array)
+for row in df.iterrows():
+    row = row[1]
+    if row[4] in degree_2_df["address"].values and row[5] in degree_2_df["address"].values:
+        decomposed_df.loc[len(decomposed_df)] = row
+
+print(decomposed_df)
+decomposed_df.to_csv("../datasource/etherscan-best-pub-trans-2-core.csv")
